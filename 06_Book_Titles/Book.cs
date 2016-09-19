921f1b61be06308a8d828efe6071a8449d5f7913
Elitsa Marinovska <Elitsa Marinovska@DESKTOP-03FMA85>
2016-09-19 15:11:38 +0200

    Add project files.

---
+++ 06_Book_Titles/Book.cs
@@ -0,0 +1,36 @@
+﻿namespace _06_Book_Titles
+{
+    internal class Book
+    {
+        public string wordsToReturn = "";
+        public string Title
+        {
+            get
+            {
+                return wordsToReturn;
+            }
+            internal set
+            {
+                string[] words = value.Split(' ');
+                bool firstWord = true;
+                foreach (string word in words)
+                {
+                    if ((word == "the" || word == "a" || word == "an" || word == "and" || word == "in" || word == "of" )&& firstWord == false)
+                    {
+                        wordsToReturn = wordsToReturn + " " + word;
+                    }
+                    else
+                    {
+                        wordsToReturn = wordsToReturn + " " + word.Substring(0, 1).ToUpper() + word.Substring(1);
+                        wordsToReturn = wordsToReturn.Trim();
+                    }
+                    firstWord = false;
+                }
+
+            }
+
+        }
+
+
+    }
+}
